41a044cf8f092f01dc6dee915e30c03fdca2b7bd
romain groult <romaingroultlgc@gmail.com>
2023-03-05 15:54:27 +0100

    initial commit

---
+++ src/test/java/com/pariuteam/back/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pariuteam.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
