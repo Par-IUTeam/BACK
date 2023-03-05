1421e1263126c1a6bfedc5c442b8d5e060f5c9b4
roms-grlt <romaiongroultlgc@gmail.com>
2023-03-05 20:21:19 +0100

    fix bean UserValidator.java

--- src/main/java/com/pariuteam/back/models/User.java
+++ src/main/java/com/pariuteam/back/models/User.java
@@ -1,25 +1,32 @@
 package com.pariuteam.back.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Table(name="users")
+@Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+    @Column(name="userName")
     private String userName;
+    @Column(name="userLastName")
     private String userLastName;
+    @Column(name="mail")
     private String mail;
+    @Column(name="streetNumber")
     private Integer streetNumber;
+    @Column(name="streetName")
     private String streetName;
+    @Column(name="buildingNumber")
     private String buildingNumber;
+    @Column(name="flatNumber")
     private String flatNumber;
+    @Column(name="cityName")
     private String cityName;
+    @Column(name="postalCode")
     private String postalCode;
 }
--- src/main/java/com/pariuteam/back/validators/UserValidator.java
+++ src/main/java/com/pariuteam/back/validators/UserValidator.java
@@ -1,7 +1,15 @@
 package com.pariuteam.back.validators;
 
 import com.pariuteam.back.models.User;
+import jakarta.persistence.Entity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Service;
 
+import java.beans.JavaBean;
+
+
+@Service
 public class UserValidator {
     public boolean isUserValid(User user) {
         return true;
